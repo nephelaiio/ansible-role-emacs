877f6427a5ebf313e2887836b5ae39ae31bd771b
Ted Cook <teodoro.cook@gmail.com>
2021-01-12 14:58:52 -0600

    Add ci workflow for install script

--- install.sh
+++ install.sh
@@ -61,9 +61,7 @@ else
     cp -a . "$tmpdir"
 fi
 pushd "$tmpdir/install"
-if [ -f ../requirements.yml ]; then
-    ansible-galaxy install -r ../requirements.yml --force
-fi
+ansible-galaxy install role install nephelaiio.emacs --force
 ansible-playbook --become --connection=local -i inventory playbook.yml -t install
 ansible-playbook --connection=local -i inventory playbook.yml "${POSITIONAL[@]}" -t configure -e emacs_doom_config=yes
 popd
